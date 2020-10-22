3940dfa539a0b03f0e18a159970619f1970f2413
akswwebsoft <info@aksindia.com>
2020-10-22 15:34:12 +0530

    add cart order list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 use Session;
 use Illuminate\Support\Facades\DB;
 
@@ -50,9 +51,52 @@ class ProductController extends Controller
         $products = DB::table('cart')
         ->join('products', 'cart.product_id', '=', 'products.id')
         ->where('cart.user_id', $userid)
-        ->select('products.*')
+        ->select('products.*', 'cart.id as cart_id')
         ->get();
 
         return view('cartlist', ['products'=>$products]);
+    }
+
+    public function removeCart($id){
+        Cart::destroy($id);
+        return redirect('cartlist');
+    }
+
+    public function orderNow(){
+        $userid = Session::get('user')['id'];
+         $products = DB::table('cart')
+        ->join('products', 'cart.product_id', '=', 'products.id')
+        ->where('cart.user_id', $userid)
+        ->sum('products.price');
+
+        return view('ordernow', ['total'=>$products]);
+    }
+
+    public function orderPlace(Request $req){
+            $userId = Session::get('user')['id'];
+            $allCart = Cart::where('user_id', $userId)->get();
+            foreach($allCart as $cart){
+                    $order = new Order;
+                    $order->product_id=$cart['product_id'];
+                    $order->user_id= $cart['user_id'];
+                    $order->status="pending";
+                    $order->payment_method= $req->payment;
+                    $order->payment_status="pending";
+                    $order->address=$req->address;
+                    $order->save();
+                    Cart::where('user_id', $userId)->delete();
+            }
+            // return $req->input();
+            return redirect('/');
+    }
+
+    public function myOrders(){
+        $userid = Session::get('user')['id'];
+        $orders = DB::table('orders')
+       ->join('products', 'orders.product_id', '=', 'products.id')
+       ->where('orders.user_id', $userid)
+       ->get();
+
+       return view('myorders', ['orders'=>$orders]);
     }
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -21,8 +21,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">Home</span></a></li>
-        <li><a href="#">Order</a></li>
+        <li class="active"><a href="{{URL::to('/')}}">Home <span class="sr-only">Home</span></a></li>
+        <li><a href="{{url('/myorders')}}">Order</a></li>
         
       </ul>
       <form action="search" class="navbar-form navbar-left">
